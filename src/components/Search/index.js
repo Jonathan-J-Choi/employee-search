5087a40e9a762c125cb151e75818c6c0c0200201
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-03-03 18:11:48 -0500

    fully functional

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,83 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import API from "../../utils/API";
 
 function Search(props) {
+  const[state, setState] = useState({
+    empArray: [
+      {
+        image:"",
+        name: "Tom",
+        email: "tom@tom.com",
+        dob: "05-05-95",
+        phone: "555-555-5555"
+      },
+      {
+        image: "",
+        name: "Jon",
+        email: "jon@jon.com",
+        dob: "06-06-96",
+        phone: "666-666-6666"
+      }
+    ],
+    filtArray: [
+      {
+        image:"",
+        name: "Tom",
+        email: "tom@tom.com",
+        dob: "05-05-95",
+        phone: "555-555-5555"
+      },
+      {
+        image: "",
+        name: "Jon",
+        email: "jon@jon.com",
+        dob: "06-06-96",
+        phone: "666-666-6666"
+      }
+    ]
+  })
+
+  useEffect( () => {
+    API.getEmployee().then( (data)=>{
+      console.log(data)
+      var cleanEmpArr = []
+      data.data.results.map((singleUser)=>{
+        var cleanUser = {}
+        cleanUser.name = singleUser.name.first+" "+singleUser.name.last
+        cleanUser.email = singleUser.email
+        cleanUser.phone = singleUser.phone
+        cleanUser.dob = singleUser.dob.date
+        cleanUser.image = singleUser.picture.thumbnail
+        cleanEmpArr.push(cleanUser)
+      })
+      console.log(cleanEmpArr)
+      setState({...state, filtArray:cleanEmpArr,empArray:cleanEmpArr})
+    })
+  },[])
+
+
+  const handleTyping= (e) => {
+    console.log(e.target.value)
+    console.log(e.target.value.length)
+    var newFiltArray = []
+    state.empArray.map((singleEmp) => {
+      var compareStr = ""
+      for (var i = 0; i < e.target.value.length; i++){
+        compareStr += singleEmp.name[i]
+      }
+      console.log(compareStr)
+      if (e.target.value.toLowerCase() === compareStr.toLocaleLowerCase()){
+        console.log("return for the search", singleEmp.name)
+        newFiltArray.push(singleEmp)
+      }
+    })
+
+    setState({
+      ...state,
+      filtArray:newFiltArray
+    })
+  }
+
   return (
     <div className="container">
       <br></br>
@@ -8,7 +85,7 @@ function Search(props) {
         <div className="container text-center">
           <br></br>
           <form className="form">
-            <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+            <input onChange={handleTyping} className="form-control" type="search" placeholder="Search" aria-label="Search"/>
           </form>
           <br></br>
         </div>
@@ -24,12 +101,19 @@ function Search(props) {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">{props.image}</th>
-                <td>{props.name}</td>
-                <td>{props.phone}</td>
-                <td>{props.dob}</td>
-              </tr>
+              { state.filtArray.map((singleEmp)=>{
+                  return(
+                    <tr>
+                      <th scope="row"><img src={singleEmp.image}/></th>
+                      <td>{singleEmp.name}</td>
+                      <td>{singleEmp.phone}</td>
+                      <td>{singleEmp.email}</td>
+                      <td>{singleEmp.dob}</td>
+                    </tr>
+                  )
+                })
+              }
+              
             </tbody>
           </table>
         </div>
